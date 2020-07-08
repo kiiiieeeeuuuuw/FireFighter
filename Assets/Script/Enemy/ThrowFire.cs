5c8f0cdac7074228370899d49bf5e5abe3b281af
kiiiieeeeuuuuw <quintenschelfhout@hotmail.com>
2020-07-08 14:04:48 +0200

    starting spawning of fire

---
+++ Assets/Script/Enemy/ThrowFire.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowFire : MonoBehaviour
+{
+    public int NumberOfSpawns;
+
+    public GameObject Flame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < NumberOfSpawns; i++)
+        {
+            Instantiate(Flame, transform.position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -1,6 +1,7 @@
 ﻿using Assets.Script.General;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -180,8 +181,8 @@ public class PlayerMovement : MonoBehaviour
     }
 
     void WallGlide()
-    {
-        IsTouchingFront = Physics2D.OverlapCircle(FrontalCheck.position, CheckRadius, 7);
+    {         
+        IsTouchingFront = Physics2D.OverlapCircleAll(FrontalCheck.position, CheckRadius).Any(x => x.CompareTag("Wall"));
         var move = Input.GetAxisRaw("Horizontal");
         bool WallSliding = IsTouchingFront && !isGrounded;        
         if(WallSliding)
