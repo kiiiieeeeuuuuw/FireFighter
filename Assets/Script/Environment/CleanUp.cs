b14020412b1ac13d4a4a97f300b621d80d43a1a2
kiiiieeeeuuuuw <quintenschelfhout@hotmail.com>
2020-08-13 08:21:26 +0200

    added skyline

--- Assets/Script/Environment/CleanUp.cs
+++ Assets/Script/Environment/CleanUp.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class CleanUp : MonoBehaviour
 {
-    [System.Obsolete]
+    /*[System.Obsolete]
     void OnCollisionEnter2D(Collision2D collision)
     {
+        if(collision.get)
         Application.LoadLevel(Application.loadedLevel);        
+    }*/
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
     }
 }
