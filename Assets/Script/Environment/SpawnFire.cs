0bd6085a2fc25fa3ca84e1d6a3474d56645c08cf
kiiiieeeeuuuuw <quintenschelfhout@hotmail.com>
2020-08-24 23:44:24 +0200

    added explosion to meteor

---
+++ Assets/Script/Environment/SpawnFire.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFire : MonoBehaviour
+{
+    public ParticleSystem Explosion;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Meteor"))
+        {
+            Instantiate(Explosion,collision.transform.position, Quaternion.identity, this.transform);
+            Destroy(collision.gameObject);
+        }
+    }
+}
