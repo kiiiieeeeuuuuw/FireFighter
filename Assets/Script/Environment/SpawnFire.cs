426deb6cd65ee12174cdce5f3c4b8b6f0a72f5ab
kiiiieeeeuuuuw <quintenschelfhout@hotmail.com>
2020-08-25 13:00:57 +0200

    added fire to meteorshower

--- Assets/Script/Environment/SpawnFire.cs
+++ Assets/Script/Environment/SpawnFire.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class SpawnFire : MonoBehaviour
 {
     public ParticleSystem Explosion;
+    public GameObject Fire;
+    public float delta;
+    public bool xDelta;
+    public bool yDelta;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Meteor"))
         {
-            Instantiate(Explosion,collision.transform.position, Quaternion.identity, this.transform);
+            Vector3 impactPos = collision.transform.position;
+            if (xDelta)
+                impactPos.x -= delta;
+            if (yDelta)
+                impactPos.y -= delta;
+            else return;
+            Instantiate(Explosion,impactPos, Quaternion.identity, this.transform);
+            Instantiate(Fire, impactPos, Quaternion.identity, this.transform);
             Destroy(collision.gameObject);
         }
     }
