99d51279a07cddb23ff066e893ab4d703b5861f6
kiiiieeeeuuuuw <quintenschelfhout@hotmail.com>
2020-09-24 17:08:37 +0200

    continued on rotating warning

--- Assets/ShowTrajectory.cs
+++ Assets/ShowTrajectory.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Net;
 using UnityEngine;
 
 public class ShowTrajectory : MonoBehaviour
@@ -7,7 +6,10 @@ public class ShowTrajectory : MonoBehaviour
     public Vector2 StartingPoint;
     public Vector2 TargetPoint;
 
+    public GameObject Trail;
+
     public bool draw;
+    public bool TrailShown;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +20,14 @@ public class ShowTrajectory : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (draw && !TrailShown)
+        {
+            Vector2 direction = StartingPoint - TargetPoint;
+            //Instantiate(Trail, StartingPoint, Quaternion.identity);
+            var t = Instantiate(Trail, StartingPoint, Quaternion.LookRotation(direction));
+            
+            TrailShown = true;
+        }
     }
 
     public void StartDrawing(Vector2 start, Vector2 target)
