2996c106b2cfc9b01abec4571f998d6ab5c2fd09
kiiiieeeeuuuuw <quintenschelfhout@hotmail.com>
2020-09-25 18:01:22 +0200

    started working on optimisation of particle effects

---
+++ Assets/Script/Environment/ManageActiveFires.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageActiveFires : MonoBehaviour
+{
+    public GameObject camera;
+    public GameObject[] CleanupFires;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Collect all fires
+        int k = 0;
+        int numberOfFires = transform.childCount;
+        CleanupFires = new GameObject[numberOfFires];
+        foreach(Transform t in transform)
+        {
+            CleanupFires[k] = t.gameObject;
+            k++;
+        }                
+
+        // Sort them based upon x coordinates
+        for(int i = 1; i < numberOfFires; i++)
+        {
+            var h = CleanupFires[i];
+            int j = i - 1;
+            while(j >= 0 && h.transform.position.x < CleanupFires[j].transform.position.x)
+            {
+                CleanupFires[j + 1] = CleanupFires[j];
+                j--;
+            }
+            CleanupFires[j + 1] = h;
+        }
+        
+        // Get Camera bounds
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
