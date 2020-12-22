22918d46fa25a4525abba32e27f567dc7e21a869
Quinten Schelfhout <quintenschelfhout@hotmail.com>
2020-12-22 16:38:06 +0100

    fixed button

--- Assets/Script/UIScripts/Quit.cs
+++ Assets/Script/UIScripts/Quit.cs
@@ -4,12 +4,7 @@ public class Quit : MonoBehaviour
 {
     public GameObject QuitButton;
     public ParticleSystem ButtonFire;
-
-    [Header("Colors")]
-    public Color HoverColor1;
-    public Color HoverColor2;
-    public Color IdleColor1;
-    public Color IdleColor2;
+    public ParticleSystem ButtonHoverFire;
 
     private Animator QuitButtonAC;
 
@@ -25,19 +20,15 @@ public class Quit : MonoBehaviour
 
     public void HoverEnterQuitButton()
     {
-        HandleColors(HoverColor1, HoverColor2);
+        ButtonFire.Stop();
+        ButtonHoverFire.Play();
+        Debug.Log("hover enter");
     }
 
     public void HoverLeaveQuitButton()
-    {
-        HandleColors(IdleColor1, IdleColor2);
-    }
-
-    private void HandleColors(Color color1, Color color2)
-    {
-        //var bfMain = ButtonFire.main;
-        //bfMain.startColor = new ParticleSystem.MinMaxGradient(color1, color2);
-
-        ButtonFire.startColor = color1;
+    {        
+        ButtonHoverFire.Stop();
+        ButtonFire.Play();
+        Debug.Log("hover leave");
     }
 }
