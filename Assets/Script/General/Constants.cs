35892d7e6b30da083a489d627d521d680ecefc3c
kiiiieeeeuuuuw <quintenschelfhout@hotmail.com>
2021-01-14 13:12:14 +0100

    added UI

--- Assets/Script/General/Constants.cs
+++ Assets/Script/General/Constants.cs
@@ -4,6 +4,8 @@
     {
         public static string KeyboardLayout = "Keyboard";
         public static string Azerty = "AZERTY";
+        public static string AzertyInput = "Z, Q, S, D";
         public static string Qwerty = "QWERTY";
+        public static string QwertyInput = "W, A, S, D";
     }
 }
--- Assets/Script/UIScripts/MainMenu.cs
+++ Assets/Script/UIScripts/MainMenu.cs
@@ -7,12 +7,18 @@ public class MainMenu : MonoBehaviour
 {
     public string NewGameScene;
     public Text KeyboardLayoutPreference;
+    public Text InputText;
 
     public void Start()
     {
         if (!PlayerPrefs.HasKey(Constants.KeyboardLayout))
-            PlayerPrefs.SetString(Constants.KeyboardLayout, Constants.Azerty);        
-        KeyboardLayoutPreference.text = PlayerPrefs.GetString(Constants.KeyboardLayout);
+            PlayerPrefs.SetString(Constants.KeyboardLayout, Constants.Azerty);
+        var pref = PlayerPrefs.GetString(Constants.KeyboardLayout);
+        KeyboardLayoutPreference.text = pref;
+        if (pref == Constants.Azerty)
+            InputText.text = Constants.AzertyInput;
+        else
+            InputText.text = Constants.QwertyInput;
     }
     public void NewGame()
     {
@@ -30,6 +36,12 @@ public class MainMenu : MonoBehaviour
             PlayerPrefs.SetString(Constants.KeyboardLayout, Constants.Qwerty);
         else
             PlayerPrefs.SetString(Constants.KeyboardLayout, Constants.Azerty);
-        KeyboardLayoutPreference.text = PlayerPrefs.GetString(Constants.KeyboardLayout);
+
+        var pref = PlayerPrefs.GetString(Constants.KeyboardLayout);
+        KeyboardLayoutPreference.text = pref;
+        if (pref == Constants.Azerty)
+            InputText.text = Constants.AzertyInput;
+        else
+            InputText.text = Constants.QwertyInput;
     }
 }
