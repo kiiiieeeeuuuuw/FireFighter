60b9e206cbd89527ae8b8cd37e27c69f6d83315f
kiiiieeeeuuuuw <quintenschelfhout@hotmail.com>
2021-01-21 17:44:21 +0100

    started button selection

---
+++ Assets/MainMenuButtonSelector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuButtonSelector : MonoBehaviour
+{
+    public List<GameObject> buttons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
